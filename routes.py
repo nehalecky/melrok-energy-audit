7a2ef03142bb2dc5b2248f782a02fc44c0da592c
Nicholaus E. Halecky <nehalecky@gmail.com>
2013-08-22 12:51:23 -0700

    ENH: Add validation to contact form

--- routes.py
+++ routes.py
@@ -1,28 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from forms import ContactForm
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-  form = ContactForm()
+    form = ContactForm()
   
-  if request.method == 'POST':
-    return 'Form posted.'
-  
-  elif request.method == 'GET':
-    return render_template('contact.html', form=form)
-  
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            return 'Form posted.'
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)    
+
 
 if __name__ == '__main__':
   app.run(debug=True)
