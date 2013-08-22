c20f98ed34198709c389dd8196d6243279d14b5a
Nicholaus E. Halecky <nehalecky@gmail.com>
2013-08-21 21:49:55 -0700

    ENH: Add about page to templates

--- routes.py
+++ routes.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
